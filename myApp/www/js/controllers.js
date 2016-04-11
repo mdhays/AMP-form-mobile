2a44d8e73493a20bda556f44a877b33fce1aa610
Matt Hays <mdhays1995@gmail.com>
2016-04-11 18:25:28 -0500

    Text for descriptions now wraps, continuing to add all fields.

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -24,11 +24,6 @@ angular.module('starter.controllers', [])
 // Added for the form view.
 .controller('NewCaseCtrl', function($scope) {
 
-  $scope.checkChanged = function(item){
-    if(item.winner) $scope.checked++;
-    else $scope.checked--;
-  }
-
   $scope.ASA10 = {
     ASA10A: 'No serious adverse event',
     ASA10B: 'Serious adverse event'
@@ -37,6 +32,19 @@ angular.module('starter.controllers', [])
   $scope.ASA11 = {
     ASA11A: 'PT experienced cardiac arrest',
     ASA11B: 'No cardiac arrest'
+  }
+
+  $scope.ASA12 = {
+    ASA12A: 'Patient died',
+    ASA12B: 'Patient did not die'
+  }
+
+  $scope.ASA13 = {
+    ASA13H: 'Procedure under General Anesthesia by an SGA or ETT',
+    ASA13J: 'SGA or ETT was removed in OR or PACU',
+    ASA13A: 'Pt required new airway management prior to PACU D/C',
+    ASA13B: 'Pt did not require new airway management',
+    ASA13G: 'Pt received a planned trial of extubation'
   }
 
 })
