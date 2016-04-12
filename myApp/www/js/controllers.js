04612eff1230f516c41862c568c3d17c5dd44a4a
Matt Hays <mdhays1995@gmail.com>
2016-04-12 16:36:34 -0500

    Finished initial form fields.

--- myApp/www/js/controllers.js
+++ myApp/www/js/controllers.js
@@ -66,6 +66,41 @@ angular.module('starter.controllers', [])
     ASA35D: 'Elective surgical procedure was not cancelled'
   }
 
+  $scope.PQRS76 = {
+    _6030F: 'All elements of Maximal Sterile Barrier techniques followed',
+    _6030F_1P: 'Medical Reason for not following Maximal Sterile Barrier techniques',
+    _6030F_8P: 'Maximal Sterile Barrier techniques not followed, not otherwise specified'
+  }
+
+  $scope.PQRS404 = {
+    G9642: 'Current Cigarrete Smoker',
+    G9643: 'Elective Surgery',
+    G9497: 'Seen preoperatively by anesthesiologist or proxy PRIOR TO DAY OF SURGERY and advised not to smoke before surgery',
+    G9644: 'Pt abstained from smoking',
+    G9645: 'Pt DID NOT abstain from smoking on the day of the surgery'
+  }
+
+  $scope.PQRS424 = {
+    _4255F: 'Anesthesia of 60 min. duration or longer',
+    _4559F: 'Temp of at least 35.5 degrees Celcius taken 30 min. before or 15 min. immediately following anesthesia end time',
+    _4559F_1P: 'Medical reason(s) for not achieving at least 1 body temperature measurement equal to or greater than 35.5 degrees Celcius within the 30 min. immediately before or the 15 min. immediately after anesthesia end time (e.g, Emergency cases, Intentional hypothermia, etc.)',
+    _4559F_8P: 'Temp of 35.5 degrees Celcius not recorded 30 min. before or 15 min. immediately following anesthesia end time, not otherwise specified'
+  }
+
+  $scope.PQRS426 = {
+    G9656: 'Pt transferred directly from anesthetizing location to PACU',
+    G9657: 'Transfer of care during an anesthetic to the ICU',
+    G9655: 'Transfer of care checklist or protocol used',
+    G9658: 'Transfer of care checklist or protocol NOT used'
+  }
+
+  $scope.PQRS427 = {
+    _0581F: 'Pt transferred directly from anesthetizing location to critical care unit',
+    _0583F: 'Transfer of care checklist or protocol used',
+    _0583F_8P: 'Transfer of care checklist or protocol NOT used'
+  }
+
+
 })
 
 
